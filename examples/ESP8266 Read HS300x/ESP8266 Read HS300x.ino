bab4397b8b6d05f325607b341ca73ba2dc2f0f87
k2biru <fahrizal.hari@gmail.com>
2019-08-21 16:40:50 +0700

    add timer

--- examples/ESP8266 Read HS300x/ESP8266 Read HS300x.ino
+++ examples/ESP8266 Read HS300x/ESP8266 Read HS300x.ino
@@ -3,8 +3,11 @@
 #include <HS300xlib.h>
 #define SDA     12
 #define SCL     2
+#define UPDATE_TIME 10000
 
 HS300xlib hs300x;
+
+uint32_t CounterSuhu =0;
 
 
 void setup(){
@@ -18,11 +21,14 @@ void setup(){
 
 void loop(){
 
+  uint32_t now = millis();
+	if (now - CounterSuhu > UPDATE_TIME) {
     uint8 respon = hs300x.MeasurementReq();
     if(respon){
+        CounterSuhu = now;
+        //Serial.print("status ="); Serial.println(respon);
         Serial.print("Humidity ");Serial.print(hs300x.getHumidity());
         Serial.print(" Temperature ");Serial.println(hs300x.getTemperatureC());
     }
-    //Serial.print("Respon ");Serial.println(respon);
-    delay(100);
+  }
 }
