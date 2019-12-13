52c39e57cc67adc9466698642c788d94199dfa3e
k2biru <fahrizal.hari@gmail.com>
2019-12-13 09:01:36 +0700

    Add Info

--- examples/ESP8266 Read HS300x/ESP8266 Read HS300x.ino
+++ examples/ESP8266 Read HS300x/ESP8266 Read HS300x.ino
@@ -1,34 +1,33 @@
 #include <Arduino.h>
-#include <Wire.h>
+#include <Wire.h>    // Using Wire Library
 #include <HS300xlib.h>
 #define SDA     12
 #define SCL     2
-#define UPDATE_TIME 10000
+#define UPDATE_TIME 10000    // get data every 10 sec
 
-HS300xlib hs300x;
+HS300xlib hs300x;  // create class
 
-uint32_t CounterSuhu =0;
-
-
+uint32_t CounterTemperature = 0;
 void setup(){
   Serial.begin(115200);
   Serial.println();
   Serial.println();
   Serial.println("Im Ready");
 
-  Wire.begin(SDA,SCL);
+  Wire.begin(SDA,SCL); // begin I2C 
+  // Wire.begin();  // use Wire.begin(); for AVR Base (Arduino mini, Uno)            
 }
 
 void loop(){
 
   uint32_t now = millis();
-	if (now - CounterSuhu > UPDATE_TIME) {
+	if (now - CounterTemperature > UPDATE_TIME) {
     uint8 respon = hs300x.MeasurementReq();
     if(respon){
-        CounterSuhu = now;
-        //Serial.print("status ="); Serial.println(respon);
+        CounterTemperature = now;
+        //Serial.print("status ="); Serial.println(respon);  // respon to see Sensor condition
         Serial.print("Humidity ");Serial.print(hs300x.getHumidity());
-        Serial.print(" Temperature ");Serial.println(hs300x.getTemperatureC());
+        Serial.print("Temperature ");Serial.println(hs300x.getTemperatureC());
     }
   }
 }
